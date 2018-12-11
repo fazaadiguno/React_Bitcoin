a1395d4606515bdd4ae79153502700dad8fcdaea
fazaadiguno <faza.adiguno@gmail.com>
2018-12-11 15:16:54 +0700

    Add files via upload

---
+++ precache-manifest.db648379d8c70fbc6d7cc2d4558d78cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "179a25d8f8aed2668f5a",
+    "url": "/React_Bitcoin/static/js/runtime~main.179a25d8.js"
+  },
+  {
+    "revision": "9abecacecdee08e5e772",
+    "url": "/React_Bitcoin/static/js/main.9abecace.chunk.js"
+  },
+  {
+    "revision": "2cbde948e7e9fc6d28ad",
+    "url": "/React_Bitcoin/static/js/1.2cbde948.chunk.js"
+  },
+  {
+    "revision": "9abecacecdee08e5e772",
+    "url": "/React_Bitcoin/static/css/main.04f03cc0.chunk.css"
+  },
+  {
+    "revision": "040e3b72ae945365d97a3b6080d20043",
+    "url": "/React_Bitcoin/index.html"
+  }
+];
